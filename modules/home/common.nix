e3768aa9348b8f2dececcf49655ccf702181ff4a
Kevin Ferneding <email@kevinferneding.xyz>
2026-01-10 00:35:43 +0100

    gtk theme

--- modules/home/common.nix
+++ modules/home/common.nix
@@ -1,14 +1,34 @@
-{ pkgs, ... }:
-
-{
+{pkgs, ...}: {
   home.packages = with pkgs; [
     age
     sops
+    colloid-gtk-theme
+    colloid-icon-theme
   ];
 
   catppuccin = {
     enable = true;
     flavor = "mocha";
+    cursors.enable = true;
+    catppuccin.cursors.accent = "mauve"
+  };
+  gtk = {
+    enable = true;
+
+    theme = {
+      name = "Colloid-Dark-Catppuccin"; # or another variant
+      package = pkgs.colloid-gtk-theme;
+    };
+
+    iconTheme = {
+      name = "Colloid-Catppuccin";
+      package = pkgs.colloid-icon-theme;
+    };
+
+    cursorTheme = {
+      name = "Colloid-Catppuccin";
+      package = pkgs.colloid-cursors;
+    };
   };
 
   programs.git = {
