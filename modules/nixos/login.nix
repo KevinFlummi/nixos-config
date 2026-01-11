72bac35452aa241872a9dcdee32443f45e308830
Kevin Ferneding <email@kevinferneding.xyz>
2026-01-11 22:20:54 +0100

    different login manager

--- modules/nixos/login.nix
+++ modules/nixos/login.nix
@@ -1,8 +1,13 @@
-{pkgs, ...}: {
-  # minimalist login manager
-  services.greetd.enable = true;
-  services.greetd.settings.default_session = {
-    command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd ${pkgs.niri}/bin/niri-session";
-    user = "greeter";
+{
+  services.xserver.enable = true;
+
+  services.displayManager.ly = {
+    enable = true;
+    settings = {
+      animate = true;
+      clock = true;
+      save = true;
+      default_user = "kevin";
+    };
   };
 }
