4ec982bb6822ac9ff5b614dd221419a97118fd36
Kevin Ferneding <email@kevinferneding.xyz>
2026-01-11 16:29:52 +0100

    no protonup

--- modules/home/gaming.nix
+++ modules/home/gaming.nix
@@ -1,11 +1,6 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     mangohud
-    protonup
     prismlauncher
   ];
-
-  home.sessionVariables = {
-    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
-  };
 }
