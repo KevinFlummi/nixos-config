4b0260bd874da8129b8038ccc7e01cc2eaea7d12
Kevin Ferneding <email@kevinferneding.xyz>
2026-01-20 14:48:20 +0100

    keepass gui conf

--- modules/home/desktop.nix
+++ modules/home/desktop.nix
@@ -6,14 +6,12 @@
   home.packages = with pkgs; [
     niri # wm
     xwayland-satellite # port x-dependencies
-    mpv # videos
     nsxiv # images
-    keepassxc # pw manager
-    thunderbird # email
     thunar # file explorer
     thunar-archive-plugin # zip util
     thunar-media-tags-plugin # see media metadata
     thunar-volman # mounting
+    thunderbird # email client
     gpu-screen-recorder # screen recording
     spotify # music player
   ];
@@ -21,6 +19,30 @@
   programs.noctalia-shell = {
     enable = true;
   };
+
+  programs.mpv.enable = true; # media player
+
+  # todo: move thunderbird config to homemgr
+  #programs.thunderbird.enable = true; # email client
+
+  # password manager
+  programs.keepassxc = {
+    enable = true;
+    autostart = true;
+    settings = {
+      FdoSecrets.Enabled = true;
+      Browser = {
+        Enabled = true;
+      };
+      GUI = {
+        AdvancedSettings = true;
+        ApplicationTheme = "dark";
+        CompactMode = true;
+        HidePasswords = true;
+      };
+    };
+  };
+  xdg.autostart.enable = true;
 
   xdg.configFile."niri".source = "${dotfiles}/niri";
   xdg.configFile."noctalia".source = "${dotfiles}/noctalia";
