72efc18f7aa3931c2fed093bb2bf6f2740042349
Kevin Ferneding <email@kevinferneding.xyz>
2026-01-23 16:08:53 +0100

    tuigreet standalone

--- modules/nixos/login.nix
+++ modules/nixos/login.nix
@@ -4,7 +4,7 @@
     settings = {
       default_session = {
         command = ''
-          ${pkgs.greetd.tuigreet}/bin/tuigreet \
+          ${pkgs.tuigreet}/bin/tuigreet \
           --time \
           --asterisks \
           --user-menu \
