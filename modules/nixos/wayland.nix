b66824da116147765b37f3b76674922381666cc0
Kevin Ferneding <email@kevinferneding.xyz>
2026-01-26 15:57:19 +0100

    gnome & gtk xdg portal support

--- modules/nixos/wayland.nix
+++ modules/nixos/wayland.nix
@@ -1,17 +1,26 @@
 {pkgs, ...}: {
+  environment.systemPackages = with pkgs; [
+    xdg-desktop-portal-gtk
+    xdg-desktop-portal-wlr
+    xdg-desktop-portal-gnome
+  ];
   # wayland ports
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   programs.xwayland.enable = true;
   xdg.portal = {
     enable = true;
     extraPortals = [
+      pkgs.xdg-desktop-portal-gnome
       pkgs.xdg-desktop-portal-wlr
       pkgs.xdg-desktop-portal-gtk
-      pkgs.xdg-desktop-portal-gnome
     ];
     config = {
       common = {
-        default = ["wlr"];
+        default = [
+          "gnome"
+          "gtk"
+          "wlr"
+        ];
       };
     };
   };
