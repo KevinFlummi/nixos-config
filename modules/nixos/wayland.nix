5c81df9e5fe714fc4970aeaf79989e0d61ab0495
Kevin Ferneding <email@kevinferneding.xyz>
2026-01-26 14:43:36 +0100

    screencast support

--- modules/nixos/wayland.nix
+++ modules/nixos/wayland.nix
@@ -6,6 +6,8 @@
     enable = true;
     extraPortals = [
       pkgs.xdg-desktop-portal-wlr
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal-gnome
     ];
     config = {
       common = {
