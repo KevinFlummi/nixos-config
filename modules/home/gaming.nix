ed1fa2cf78ce75262bf31b37cea0292019f46cff
Kevin Ferneding <email@kevinferneding.xyz>
2026-02-09 12:14:10 +0100

    mangohud conf

--- modules/home/gaming.nix
+++ modules/home/gaming.nix
@@ -1,6 +1,12 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  dotfiles,
+  ...
+}: {
   home.packages = with pkgs; [
     mangohud
     prismlauncher
   ];
+
+  xdg.configFile."MangoHud".source = "${dotfiles}/MangoHud";
 }
